1bca9b78417c76ab8438d302aed57e62801a327a
John Doe <preyaizman@gmail.com>
2021-09-23 18:31:09 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+import './scss/styles.scss';
+import Splide from '@splidejs/splide';
+
+
+new Splide( '#banners', {
+    arrows: false,
+} ).mount();
